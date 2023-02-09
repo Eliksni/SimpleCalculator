c53b2d19f8a3e26b55880d9a8fe2031f121b03d8
Eliksni <Ipodmaster2000@gmail.com>
2023-02-08 20:16:17 -0800

    Created separate scientific calculator functionality (still identical to simple calc)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import SimpleCalculator from './simple-calculator.js'
 import ScientificCalculator from './scientific-calculator.js'
 
+// Simple calculator button initialization
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -8,8 +9,6 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
-const normalButton = document.querySelector('[data-normal]')
-const scientificButton = document.querySelector('[data-scientific]')
 
 const calculator = new SimpleCalculator(previousOperandTextElement, currentOperandTextElement)
 
@@ -42,12 +41,60 @@ deleteButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
+// Scientific calculator button initialization
+const sciNumberButtons = document.querySelectorAll('[data-sci-number]')
+const sciOperationButtons = document.querySelectorAll('[data-sci-operation]')
+const sciEqualsButton = document.querySelector('[data-sci-equals]')
+const sciDeleteButton = document.querySelector('[data-sci-delete]')
+const sciAllClearButton = document.querySelector('[data-sci-all-clear]')
+const sciPreviousOperandTextElement = document.querySelector('[data-sci-previous-operand]')
+const sciCurrentOperandTextElement = document.querySelector('[data-sci-current-operand]')
+
+const sciCalculator = new ScientificCalculator(sciPreviousOperandTextElement, sciCurrentOperandTextElement)
+
+sciNumberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        sciCalculator.appendNumber(button.innerText)
+        sciCalculator.updateDisplay()
+    })
+})
+
+sciOperationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        sciCalculator.chooseOperation(button.innerText)
+        sciCalculator.updateDisplay()
+    })
+})
+
+sciEqualsButton.addEventListener('click', button => {
+    sciCalculator.compute()
+    sciCalculator.updateDisplay()
+})
+
+sciAllClearButton.addEventListener('click', button => {
+    sciCalculator.clear()
+    sciCalculator.updateDisplay()
+})
+
+sciDeleteButton.addEventListener('click', button => {
+    sciCalculator.delete()
+    sciCalculator.updateDisplay()
+})
+
+// Selector buttons intialization
+const normalButton = document.querySelector('[data-normal]')
+const scientificButton = document.querySelector('[data-scientific]')
+
 normalButton.addEventListener('click', button => {
-    document.getElementById('scicalc').style.display = "none";
+    document.getElementById('scicalc').style.display = "none"
+    normalButton.style.backgroundColor = "rgba(255, 255, 255, 0.75)"
     document.getElementById('simplecalc').style.display = "grid"
+    scientificButton.style.backgroundColor = "rgba(255, 255, 255, 0)"
 })
 
 scientificButton.addEventListener('click', button => {
-    document.getElementById('simplecalc').style.display = "none";
+    document.getElementById('simplecalc').style.display = "none"
+    scientificButton.style.backgroundColor = "rgba(255, 255, 255, 0.75)"
     document.getElementById('scicalc').style.display = "grid"
+    normalButton.style.backgroundColor = "rgba(255, 255, 255, 0)"
 })
